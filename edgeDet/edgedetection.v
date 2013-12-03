40330a177e252ba21e3c868e06e570fb65be7226
dkang9322 <wkang@mit.edu>
2013-12-03 17:55:03 -0500

    Finally working with cols=640; now actually testing with full implementation

--- edgeDet/edgedetection.v
+++ edgeDet/edgedetection.v
@@ -52,17 +52,20 @@ module edgedetection(
    /*assign edgeoutputsel={sr_grayout, sr_grayout, sr_grayout};*/
 
 
-   assign select = 1;
-   shiftregister #(.cols(640)) shifter(clock,hcount,sr_grayout,matrixout);
+   /* Stage ii Debugging */   
    /* Want to output the last element of row3 to get one pixel
     value. This is to see the appropriate amount of latency */
+   /*
+   assign select = 1;
+   shiftregister #(.cols(640)) shifter(clock,hcount,sr_grayout,matrixout);
    // With cols = 13, should see approximately double the distance
    wire [7:0] 	   shifted_gs = matrixout[23:16];
    
    assign edgeoutputsel = {shifted_gs, shifted_gs, shifted_gs};
+   */
 
    /* Actual Full Implementation */
-   /*
+
    shiftregister shifter(clock,hcount,sr_grayout,matrixout);
    
    sobel edgedetect(clock,matrixout[71:64],matrixout[63:56],
@@ -70,7 +73,7 @@ module edgedetection(
 		    matrixout[31:24],matrixout[23:16],matrixout[15:8],
 		    matrixout[7:0],swi,edgeoutputsobel);
    selectbit selector(clock,edgeoutputsobel,edgeoutputsel,select);
-    */
+
 
    
 
